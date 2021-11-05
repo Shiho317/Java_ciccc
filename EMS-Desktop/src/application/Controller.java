c8274735efbacf561d44946e9c2f668bee16aaf1
Shiho Nagano <shiho@nagano.com>
2021-11-05 11:03:41 -0700

    add adminApp

--- EMS-Desktop/src/application/Controller.java
+++ EMS-Desktop/src/application/Controller.java
@@ -1,19 +1,25 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import adminApp.AdminController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class Controller implements Initializable{
+public class Controller implements Initializable {
 	
-LoginModel loginModel = new LoginModel();
+	LoginModel loginModel = new LoginModel();
 	
 	@FXML
 	private Label dbStatus;
@@ -42,7 +48,13 @@ LoginModel loginModel = new LoginModel();
 		
 		try {
 			if(this.loginModel.isLogin(this.username.getText(), this.password.getText())) {
-				this.loginStatus.setText("You have successfully logged in");
+//				this.loginStatus.setText("You have successfully logged in");
+				
+				Stage stage = (Stage) this.loginBtn.getScene().getWindow();
+				stage.close();
+				
+				adminLogin();
+				
 			}else {
 				this.loginStatus.setText("Wrong Credentials");
 			}
@@ -50,5 +62,36 @@ LoginModel loginModel = new LoginModel();
 			// TODO: handle exception
 		}
 	}
+	
+	public void adminLogin() {
+		
+		try {
+			
+			Stage userStage = new Stage();
+			FXMLLoader adminLoader = new FXMLLoader();
+			Pane root = (Pane) adminLoader.load(getClass().getResource("/adminApp/Admin.fxml").openStream());
+			
+			AdminController adminController = (AdminController) adminLoader.getController();
+			
+			Scene scene = new Scene(root);
+			scene.getRoot().setStyle("-fx-font-family: 'sans-serif'");
+			userStage.setScene(scene);
+			userStage.setTitle("Admin Dashboard");
+			userStage.setResizable(false);
+			userStage.show();
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
 
 }
